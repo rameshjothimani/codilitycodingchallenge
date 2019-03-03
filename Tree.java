e1d71f177b0e221ab5c873ab4901824ad026443e
rameshjothimani <ramesh.iit19@gmail.com>
2019-03-03 17:21:14 +0100

    Initial version

--- Tree.java
+++ Tree.java
@@ -1,14 +1,46 @@
 package com.codility.codingchallenge;
 
 
+//Find the number of visible nodes (values of child nodes should be greater than root node)
+class Tree{
 
-class Solution {
-    public int solution(Tree T) {
-        return isVisible(T, Integer.MIN_VALUE);
+    //structure of binary tree
+    static class Node {
+        Node left;
+        Node right;
+        int x; //Root node
+    };
+
+
+    //Create a new node
+    static Node newNode(int key)
+    {
+        Node node = new Node();
+        node.left = node.right = null;
+        node.x = key;
+        return node;
+    }
+
+    public static void main(String args[])
+    {
+
+        Node root = newNode(5);
+        root.left = newNode(3);
+        root.right = newNode(10);
+        root.left.left = newNode(20);
+        root.left.right = newNode(21);
+        root.right.left = newNode(1);
+
+        System.out.println(findVisibleNodes(root));
+    }
+
+    private static int findVisibleNodes(Node root) {
+
+        return isVisible(root, Integer.MIN_VALUE);
     }
 
 
-    private int isVisible(Tree root, int maxVal) {
+    private static int isVisible(Node root, int maxVal) {
 
         if(root == null) {
             return 0;
@@ -21,24 +53,7 @@ class Solution {
             maxVal = root.x;
         }
 
-        return count + isVisible(root.l, maxVal) + isVisible(root.r, maxVal);
-    }
-
-}
-
-
-
-
-public class Tree {
-    public int x;
-    public Tree l;
-    public Tree r;
-
-
-    public static void main(String args[]){
-
-        Solution solution=new Solution();
-        solution.solution(l);
+        return count + isVisible(root.left, maxVal) + isVisible(root.right, maxVal);
     }
 
 }
