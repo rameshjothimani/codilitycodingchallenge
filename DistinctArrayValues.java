a1cca9bfeb863993294f02422f415eaf187df005
rameshjothimani <ramesh.iit19@gmail.com>
2019-04-28 22:26:00 +0200

    DistinctArrayValues : Added

---
+++ DistinctArrayValues.java
@@ -0,0 +1,33 @@
+package com.codility.codingchallenge;
+
+import java.util.Arrays;
+
+//Number of distinct values in array
+public class DistinctArrayValues {
+
+    public static void main(String args[]){
+        int [] input={2,1,1,2,3,1};
+        DistinctArrayValues distinctArrayValues=new DistinctArrayValues();
+        distinctArrayValues.findDistinct(input);
+
+    }
+
+    public void findDistinct(int[] input){
+
+        Arrays.sort(input);
+        int length=input.length;
+        int distinctCount=0;
+
+        for(int i=0;i<length;i++){
+            while(i< length-1 && input[i] == input[i+1]){
+                i++;
+            }
+
+            distinctCount++;
+
+        }
+
+        System.out.println(distinctCount);
+
+    }
+}
