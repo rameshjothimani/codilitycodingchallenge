665e0a71f5925a42c8110a84e3b716d01155d48b
rameshjothimani <ramesh.iit19@gmail.com>
2019-06-10 13:49:20 +0200

    Missing Integer : Solution

---
+++ MissingInteger.java
@@ -0,0 +1,80 @@
+package com.codility.codingchallenge;
+
+//Return smallest positive integer that does not occur in the array
+public class MissingInteger {
+
+    /* Utility function that puts all non-positive
+    (0 and negative) numbers on left side of
+    arr[] and return count of such numbers */
+    static int segregate(int[] arr, int size) {
+        int j = 0;
+        int i;
+        for (i = 0; i < size; i++) {
+            if (arr[i] <= 0) {
+                int temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                // increment count of non-positive
+                // integers
+                j++;
+            }
+        }
+
+        return j;
+    }
+
+    /* Find the smallest positive missing
+    number in an array that contains
+    all positive integers */
+    static int findMissingPositive(int[] arr, int size) {
+        int i;
+
+        // Mark arr[i] as visited by making
+        // arr[arr[i] - 1] negative. Note that
+        // 1 is subtracted because index start
+        // from 0 and positive numbers start from 1
+        for (i = 0; i < size; i++) {
+            int x = Math.abs(arr[i]);
+            if (x - 1 < size && arr[x - 1] > 0)
+                arr[x - 1] = -arr[x - 1];
+        }
+
+        // Return the first index value at which
+        // is positive
+        for (i = 0; i < size; i++)
+            if (arr[i] > 0)
+                return i + 1; // 1 is added becuase indexes
+        // start from 0
+
+        return size + 1;
+    }
+
+    /* Find the smallest positive missing
+    number in an array that contains
+    both positive and negative integers */
+    static int findMissing(int[] arr, int size) {
+        // First separate positive and
+        // negative numbers
+        int shift = segregate(arr, size);
+        int[] arr2 = new int[size - shift];
+        int j = 0;
+        for (int i = shift; i < size; i++) {
+            arr2[j] = arr[i];
+            j++;
+        }
+        // Shift the array and call
+        // findMissingPositive for
+        // positive part
+        return findMissingPositive(arr2, j);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, 6, 4, 2,1};
+        int missing = findMissing(arr, arr.length);
+        System.out.println("The smallest positive missing number is " +
+                missing);
+    }
+
+
+}
